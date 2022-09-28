755cb943d5951a3fdd1ac8fea1d2912d6dc8672c
Jonny <jonny.howle@gmail.com>
2022-09-28 16:23:11 -0600

    commit tree b15cf6516f5ccab13aac3c24dc7802ce28dc9505

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+  attr_reader :equilateral, :isosceles, :scalene
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || c == a
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate
+    if a <= 0 || b <= 0 || c <= 0
+      raise TriangleError
+    elsif a + b <= c || a + c <= b || b + c <= a
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid Triangle"
+    end
+  end
+
 end
+
+equilateral = Triangle.new(4, 4, 4)
+equilateral.kind
